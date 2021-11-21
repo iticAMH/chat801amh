b712fcd67d11981132d3fd96249e647f1bb05d6f
iticAMH <94078410+iticAMH@users.noreply.github.com>
2021-11-21 17:12:37 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+ apiKey: "AIzaSyB_1tckp4yWTRoEZt7qUvptiTdtz9-WaPY",
+  authDomain: "chat801amh.firebaseapp.com",
+  projectId: "chat801amh",
+  storageBucket: "chat801amh.appspot.com",
+  messagingSenderId: "620225560968",
+  appId: "1:620225560968:web:3b24f9c21ee930bd443a3d",
+  measurementId: "G-7L0DEJ19RF"
+    });
